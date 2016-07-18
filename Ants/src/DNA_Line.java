dd45dde8ee5a1f1b2198d88fbbaa5bdd63a56dc7
Bayley Millar <bmillar@oucs1444.otago.ac.nz>
2016-07-18 17:37:51 +1200

    testing commit

--- Ants/src/DNA_Line.java
+++ Ants/src/DNA_Line.java
@@ -7,21 +7,40 @@ public class DNA_Line {
 	private char state;
 	private char[] compassDirs;
 	private char[] statesAfterAnt;
-
+	
+	/**
+	 * This method collects the DNA line
+	 * @param state: is the inputed state of type char
+	 * @param compassDirs: is the sequence of the compass directions
+	 * @param statesAfterAnt: sequence representing the state
+	 * of the poin after the ant leaves
+	 */
 	public DNA_Line(char state, char[] compassDirs, char[] statesAfterAnt) {
 		this.state = state;
 		this.compassDirs = compassDirs;
 		this.statesAfterAnt = statesAfterAnt;
 	}
-
+	
+	/** 
+	 *getter: gets the current state
+	 * @return the state
+	 */
 	public char getState() {
 		return state;
 	}
-
+	
+	/**
+	 *  getter: gets the compass of type char[]
+	 * @return returns the compass
+	 */
 	public char[] getCompassDirs() {
 		return compassDirs;
 	}
-
+	
+	/**
+	 * getter: gets the states after the ant of type char[]
+	 * @return returns the states after the Ant
+	 */
 	public char[] getStatesAfterAnt() {
 		return statesAfterAnt;
 	}
