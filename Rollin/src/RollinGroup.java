8547c7ee8fa9eb60adcf38bdadee2911ef7daa28
Reuben Storr <rstorr95@gmail.com>
2016-07-25 21:02:00 +1200

    Create Rollin (#4)
    * Title
    
    * First attempt at improve on rolling odds
    
    * Remove debugging print statements

--- Rollin/src/RollinGroup.java
+++ Rollin/src/RollinGroup.java
@@ -2,21 +2,50 @@ public class RollinGroup extends Rollin {
 
 	@Override
 	public int decideRoll(int roll) {
-		/*
-		 * Can work out probability of next roll based on previous rolls.
-		 */
-		return rng.nextInt(diceInGame + 2);
+		int[] countOfFaces = new int[diceInGame];
+		int posToThrow = 0;
+		int groupsOfTwo = 0;
 
+		countOfFaces[roll-1]++;
+
+		for (int i = 0; i < dice.length; i++){
+			countOfFaces[dice[i]-1]++;
+		}
+
+		outer:
+		for(int i = 0; i < countOfFaces.length; i++){
+			if (countOfFaces[i] < 2) {
+				for (int j = 0; j < dice.length; j++){
+					if (i+1 == dice[j]){
+						posToThrow = j;
+						break outer;
+					}
+				}
+			} else {
+				groupsOfTwo++;
+				if (groupsOfTwo == 3){
+					return rng.nextInt(diceInGame);
+				}
+			}
+		}
+
+		return posToThrow;
 	}
 
 	public static void main(String[] args) {
-		Rollin test = new RollinGroup();
-		int turns = 0;
-		while (!test.isComplete()) {
-			test.turn();
-			turns++;
+		int scenarios = 1000;
+		int turnsTotal = 0;
+		int i = 0;
+		while (i < scenarios) {
+			Rollin test = new RollinGroup();
+			int turns = 0;
+			while (!test.isComplete()) {
+				test.turn();
+				turns++;
+			}
+			System.out.println(turns);
+			i++;
+			turnsTotal += turns;
 		}
-		System.out.println(turns);
-	}
-
+}
 }
