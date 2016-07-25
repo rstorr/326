b42543cd7326da91a99a9c1da42bb43fbad8bfff
Bayley Millar <bmillar@oucs1513.otago.ac.nz>
2016-07-25 16:59:04 +1200

    Title

--- Rollin/src/RollinGroup.java
+++ Rollin/src/RollinGroup.java
@@ -2,10 +2,35 @@ public class RollinGroup extends Rollin {
 
 	@Override
 	public int decideRoll(int roll) {
-		/*
-		 * Can work out probability of next roll based on previous rolls.
-		 */
-		return rng.nextInt(diceInGame + 2);
+		int[] countOfFaces = new int[diceInGame];
+		int[] posToKeep = new int[diceInGame];
+				
+		countOfFaces[roll-1]++;
+		
+		for (int i = 0; i < dice.length; i++){
+			countOfFaces[dice[i]-1]++;
+		}
+		
+		
+		for(int i = 0; i < countOfFaces.length; i++){
+			if (countOfFaces[i] == 2) {
+				for (int j = 0; j < dice.length; j++){
+					if (dice[j]-1 == i){
+						posToKeep[j] = 1;
+					}
+				}
+			}
+		}
+
+		
+		for(int i = 0; i < posToKeep.length; i++){
+			if (posToKeep[i] == 0){
+				System.out.println("swap at pos " + i);
+				return i;
+			}
+		}
+		
+		return 10;
 
 	}
 
