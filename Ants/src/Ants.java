f3ca7c26798a68a9c44807091a78d6e5af172ada
Reuben Storr <rstorr95@gmail.com>
2016-07-26 17:07:27 +1200

    start ants from scratch using Point (#6)

--- Ants/src/Ants.java
+++ Ants/src/Ants.java
@@ -1,56 +1,106 @@
-import java.util.Scanner;
+import java.util.*;
+import java.awt.*;
 
-/**
- * Ants class holds the main method. Takes the DNA and total steps input from stdin,
- * stores it in a Scenario object then executes the scenario.
- * @author reuben storr and Bayley Millar
- *
- */
 public class Ants {
+
+	public static boolean isInteger(String s) {
+		try {
+			Integer.parseInt(s);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		return true;
+	}
+
+	public static int getCompassInt(char dir) {
+		switch (dir) {
+		case 'N':
+			return 0;
+		case 'E':
+			return 1;
+		case 'S':
+			return 2;
+		case 'W':
+			return 3;
+		default:
+			return 0;
+		}
+	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		ArrayList<String> DNA = new ArrayList<String>();
+		int totalSteps = 0;
 		System.out.println("Enter scenarios:");
-		try{
-		while (sc.hasNextLine()) {
-			Scenario scenario = new Scenario();
-			String line = nextLine(sc);
-			while (!line.equals("") || line != null) {
-				try {
-					scenario.setTotalSteps(Integer.parseInt(line));
-					scenario.playScenario();
-					break;
 
-				} catch (NumberFormatException e) {
-					scenario.addDnaLine(line);
-					line = nextLine(sc);
-				}
+		while (sc.hasNextLine()) {
+			String line = sc.nextLine();
+			if (isInteger(line)) {
+				totalSteps = Integer.parseInt(line);
+				scenario(DNA, totalSteps);
+			} else if (line.length() == 0) {
+				DNA.clear();
+			} else if (line.charAt(0) != '#') {
+				DNA.add(line);
 			}
 		}
-		}catch(StringIndexOutOfBoundsException e){
-			System.err.println("please restart and enter valid input");
+	}
+
+	private static void scenario(ArrayList<String> DNA, int totalSteps) {
+		LinkedHashMap<String, Character> plane = new LinkedHashMap<String, Character>();
+		Point ant = new Point();
+		int count = 0;
+		int cameFrom = getCompassInt('S');
+		char nextDir = '\0';
+		char newState = '\0';
+
+		while (count < totalSteps) {
+			String key = ant.getX() + " " + ant.getY();
+			if (plane.get(key) != null) {
+				char curState = plane.get(key);
+				for (String dnaLine : DNA) {
+					if (curState == dnaLine.charAt(0)) {
+						nextDir = dnaLine.substring(2, 6).charAt(cameFrom);
+						newState = dnaLine.substring(7, 11).charAt(cameFrom);
+					}
+				}
+			} else {
+				nextDir = DNA.get(0).substring(2, 6).charAt(cameFrom);
+				newState = DNA.get(0).substring(7, 11).charAt(cameFrom);
+			}
+
+			cameFrom = getCompassInt(nextDir);
+			plane.put(key, newState);
+			moveAnt(ant, nextDir);
+			count++;
+		}
+		printScenario(DNA, totalSteps, ant);
+	}
+
+	private static void moveAnt(Point ant, char dir) {
+		switch (dir) {
+		case 'N':
+			ant.translate(0, 1);
+			break;
+		case 'E':
+			ant.translate(1, 0);
+			break;
+		case 'S':
+			ant.translate(0, -1);
+			break;
+		case 'W':
+			ant.translate(-1, 0);
+			break;
 		}
 	}
 
-	
-	
-	/**
-	 * Returns next valid line, ignoring comment lines denoted
-	 * with a '#'.
-	 * @param sc Scanner to get input from stdin.
-	 * @return Returns valid line.
-	 */
-	private static String nextLine(Scanner sc) {
-		String line = sc.nextLine();
-		
-		while (line.substring(0, 1).equals('#') && sc.hasNextLine()) {
-			line = sc.nextLine();
+	private static void printScenario(ArrayList<String> DNA, int totalSteps,
+			Point ant) {
+		for (String line : DNA) {
+			System.out.println(line);
 		}
-		
-		if (line.substring(0, 1).equals('#')) {
-			return null;
-		} else {
-			return line;
-		}
+		System.out.println(totalSteps);
+		System.out.println("# " + (int) ant.getX() + " " + (int) ant.getY() + "\n");
 	}
+
 }
